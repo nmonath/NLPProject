42937dd938ed5a27f8c22746ab37614612957092
Klim Zaporojets <klimzaporojets@Klims-MacBook-Pro.local>
2014-04-07 00:01:10 -0400

    progress

--- code/ClearNLPExecuter.py
+++ code/ClearNLPExecuter.py
@@ -1,7 +1,15 @@
 #working on it 
+import subprocess
 
-$ cd <Path>/NLPProject/Code
-$ python
->> import DependencyFileFunctions
->> dep = DependencyFileFunctions.ReadDependencyParseFile("<path>/<filename>")
->> DependencyFileFunctions.Display(dep)
+def SRLParse(filenameOrigList, filenameConfig, folderDest):
+	"""
+		This function parses a list of files using Semantic Role Labeler of ClearNLP API. 
+	"""
+	command = "java com.clearnlp.nlp.engine.NLPDecode -z srl -c <filename> -i <filepath>"
+	command = command.replace("<filename>", filenameConfig)
+	for each fileName in filenameOrigList 
+		execCmd = command.replace("<filepath>", fileName)
+		process = subprocess.Popen(execCmd.split(), stdout=subprocess.PIPE)
+		output = process.communicate()[0]
+
+
