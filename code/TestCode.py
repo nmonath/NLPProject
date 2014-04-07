66aed171f2d4f6b9a0a193a31356fa288eeed0f8
Klim Zaporojets <klimzaporojets@Klims-MacBook-Pro.local>
2014-04-07 16:01:21 -0400

    progress

--- code/TestCode.py
+++ code/TestCode.py
@@ -1,4 +1,8 @@
 #in future would be nice to have a test class to execute tests of the application
 import Dependency
+import Word2VecExecuter
 dep = Dependency.ReadDependencyParseFile("testFiles/DependencyParsed.txt")
 Dependency.Display(dep)
+Word2VecExecuter.Word2VecTrain("tools/word2vec/word2vec-read-only/text8", "tools/word2vec/word2vec-read-only/vecss.bin")
+vector = Word2VecExecuter.Word2VecGetVector("tools/word2vec/word2vec-read-only/vecss.bin", "hello")
+print(str(vector))
