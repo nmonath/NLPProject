1465ddaac5f67c3907778009806fde75a1e550e1
Nicholas Monath <nicholas.monath@gmail.com>
2014-04-13 12:31:54 -0400

    Added more feature extraction code

--- code/Dependency.py
+++ code/Dependency.py
@@ -1,6 +1,9 @@
 import numpy as np
 import os
 import tempfile
+import sys
+from scipy.sparse import csr_matrix
+from Util import *
 
 class Word:
 	"""
@@ -65,6 +68,7 @@ def ReadDependencyParseFile(filename):
 	"""
 
 	f = open(filename, 'r')
+	# Mapping from (sentence number, head id number) to a list of the complements of the head id number
 	ComplementsOfHeadInSentence = dict()
 	WordsInSentence = dict()
 	WordsInSentence[0] = []
@@ -96,6 +100,107 @@ def ReadDependencyParseFile(filename):
 			#Display(WordsInSentence[sentno])
 			Dependencies.append(Dependency(WordsInSentence[sentno][headno], WordsInSentence[sentno][compno], sentno))
 	return Dependencies
+
+
+def Features(dirname):
+	"""
+		Creates an M-by-N matrix where N is the length of the feature vector and M is number of documents
+		The documents used are all the .srl files stored in the directory dirname
+	"""
+
+	# Loads all the dependencies from all the documents
+	(all_deps, num_samples) = LoadAllDepFromFiles(dirname)
+
+	# Defines a feature vector based on the dependencies
+	feature_def = DefineFeature(all_deps)
+
+	# Convert the list of Dependency objects to an numpy array of strings
+	fast_feature_def = FastFeatures(feature_def)
+
+	# Init the features matrix, uint16 to save space. We should maybe use a sparse matrix
+	features = np.zeros((num_samples, len(feature_def)), dtype=np.uint16)
+
+	# Counter
+	count = 0;
+
+	# Clean up memory, hopefully python garbage collects
+	all_deps = 0;
+	feature_def = 0;
+
+	# Just to keep track on what has been done
+	sys.stdout.write("\nDocuments Processed: 00000")
+
+	# iterate over all the files in the directory
+	for filename in os.listdir(dirname):
+		if '.srl' in filename:
+			sys.stdout.write("\b\b\b\b\b" + str(count).zfill(5)) # print just to see code is progressing
+
+			# Extract the feature vector using the fast mechanism
+			features[count, :] = ExtractFF(fast_feature_def, FastFeatures(ReadDependencyParseFile(os.path.join(dirname, filename))))
+			count = count + 1
+	return features 
+
+def FeaturesHash(dirname):
+	"""
+		same as Features(dirname), but uses FastFeaturesHash instead of FastFeatures
+	"""
+	(all_deps, num_samples) = LoadAllDepFromFiles(dirname)
+	feature_def = DefineFeature(all_deps)
+	fast_feature_def = FastFeaturesHash(feature_def)
+	features = np.zeros((num_samples, len(feature_def)), dtype=np.uint16)
+	count = 0;
+	all_deps = 0;
+	feature_def = 0;
+	sys.stdout.write("\nDocuments Processed: 00000")
+	for filename in os.listdir(dirname):
+		if '.srl' in filename:
+			sys.stdout.write("\b\b\b\b\b" + str(count).zfill(5))
+			features[count, :] = ExtractFF(fast_feature_def, FastFeaturesHash(ReadDependencyParseFile(os.path.join(dirname, filename))))
+			count = count + 1
+	return features 
+
+
+def FastFeatures(deps):
+	"""
+		Convert a list of Dependency objects into an numpy array of strings
+	"""
+
+	FF = np.empty((len(deps), ), dtype=np.object)
+	count = 0;
+	for d in deps:
+		# Note the use of lemmatized version instead of the word form
+		FF[count] = (d.head.lemma + " " + d.complement.lemma)
+		count = count +1
+	return FF
+
+def FastFeaturesHash(deps):
+	"""
+		Convert a list of Dependency objects into an numpy array of integers (32/64 based on system)
+		Integers are hashed value of string = head + complement
+	"""
+	FF = np.empty((len(deps), ), dtype=np.dtype(int))
+	count = 0;
+	for d in deps:
+		# Note the use of the lemmatized version instead of the word fomr
+		FF[count] = (d.head.lemma + " " + d.complement.lemma).hash()
+		count = count +1
+	return FF
+
+
+def ExtractFF(ffv, allff):
+	"""
+		Inputs: ffv - the feature definition by FastFeatures or FastFeaturesHash, a 1 by N numpy array
+				allff - the fast features of a given document, a 1 by M numpy array
+
+		Output: a 1 by N vector, s.t. the i^th element of the output 
+					is number of times the i^th element of ffv appeared in allff
+	"""
+
+	# Create an M by N matrix where each row is ffv
+	ffvs = np.tile(ffv, [allff.shape[0],1])
+	# Transpose so dimensions match up and then sum, gives the count of how many times each element of ffv appears
+	return np.sum(np.transpose(ffvs) == allff, axis=1, dtype=np.uint16)
+	
 
 
 
@@ -135,6 +240,19 @@ def ExtractFeature(feature, dependencies_from_document):
 
 def KeeperPOS():
 	return ["JJ", "JJR", "JJS", "NN", "NNS", "NNP", "NNPS", "RR", "RBR", "RBS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+
+
+def LoadAllDepFromFiles(dirname):
+	deps = []
+	count = 0
+	sys.stdout.write("\nDocuments Processed: 00000")
+	for filename in os.listdir(dirname):
+		if '.srl' in filename:
+			sys.stdout.write("\b\b\b\b\b" + str(count).zfill(5))
+			count = count + 1
+			deps = deps + ReadDependencyParseFile(os.path.join(dirname, filename))
+	sys.stdout.write('\n')
+	return (deps, count)
 
 	
 def Display(dep):
