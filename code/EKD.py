d3de64f341824ce3d66605c2efe85203347a33c2
Nicholas Monath <nicholas.monath@gmail.com>
2014-04-23 16:55:49 -0400

    Added Embedding KD Tree Document Representation

---
+++ code/EKD.py
@@ -0,0 +1,24 @@
+from sklearn.neighbors import KDTree
+from sklearn.preprocessing import normalize
+import Word2VecExecuter
+import numpy as np
+
+class Document:
+
+	def __init__(self, embeddings=None, word_index=None, model=None):
+		# Normal case, build kdtree right from embeddings:
+		if (embeddings== None and (not word_index == None) and (not model == None)):
+			(idx, embeddings) = Word2VecExecuter.Word2VecLoadWordsHashTable(model, word_index)
+			embeddings = np.array(embeddings)
+			del word_index
+
+		self.kd_tree = KDTree(normalize(embeddings), leaf_size=30, metric='euclidean')
+
+
+	def distance(self, other, theta=0.5):
+		if other.__class__ == Document:
+			(d_self_to_other, i_self_to_other) = self.kd_tree.query(other.kd_tree.data, k=1, return_distance=True) 
+			del i_self_to_other
+			(d_other_to_self, i_other_to_self) = other.kd_tree.query(self.kd_tree.data, k=1, return_distance=True) 
+			del i_other_to_self
+			return np.mean(d_self_to_other)*theta + np.mean(d_other_to_self)*(1-theta)
